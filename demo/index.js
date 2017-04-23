0d573d403c9b82da0e4ebb3e45de86baca8e3d1f
Rube <353371737@qq.com>
2017-04-23 14:59:13 +0800

    change demo

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,19 @@
+var askBtn = document.querySelector('.ask');
+var askForm = document.querySelector('.ask-form');
+var t = null;
+
 document.querySelectorAll('.article-item-ask').forEach(function(item){
     item.addEventListener('click', function(){
         alert('提交成功');
     });
 });
 
-var askBtn = document.querySelector('.ask');
-var askForm = document.querySelector('.ask-form');
-var t = null;
-
 askBtn.addEventListener('click', function(e){
+    if (this.innerText === '提交咨询'){
+        alert('提交咨询成功');
+        backAskBtn();
+        return;
+    }
     this.innerText = '提交咨询';
     t && clearInterval(t) && (t = null);
     t = setInterval(function(){
@@ -25,12 +30,16 @@ askBtn.addEventListener('click', function(e){
 
 window.addEventListener('scroll', function(){
     if (this.pageYOffset > 150 && !t){
-        askBtn.innerText = '我要咨询';
-
-        var clsIdx = askForm.className.indexOf('ask-form-show');
-
-        if (clsIdx !== -1){
-            askForm.className = askForm.className.substr(0, askForm.className.indexOf('ask-form-show'));
-        }
+        backAskBtn();
     }
 });
+
+function backAskBtn(){
+    askBtn.innerText = '我要咨询';
+
+    var clsIdx = askForm.className.indexOf('ask-form-show');
+
+    if (clsIdx !== -1){
+        askForm.className = askForm.className.substr(0, askForm.className.indexOf('ask-form-show'));
+    }
+}
